846ac25c2c07ff965624c868abd5b81df9c1e117
yui666a <haiso666@gmail.com>
2024-03-29 12:24:57 +0900

    feat: ステータステーブルを作成

--- Reminder/sample/backend/db/seeds.rb
+++ Reminder/sample/backend/db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Status.create!([
+                 { name: '未完了', color: 'red', sort_order: 10 },
+                 { name: '実施中', color: 'yellow', sort_order: 20 },
+                 { name: '完了', color: 'green', sort_order: 30 }
+               ])
